abe86bb9e442345069c17f5ea16be95a5e884c98
Richard Tom <tomrichard14@gmail.com>
2018-07-03 08:50:47 -0700

    first commit

--- queries.js
+++ queries.js
@@ -7,9 +7,40 @@ const knex = require('knex')(DATABASE);
 process.stdout.write('\x1Bc');
 
 // Sample select 
-knex
-  .select()
-  .from('restaurants')
-  .limit(2)
+knex('restaurants')
+  // .select('id','name')
+  // .count()
+  // .from('restaurants')
+  .where({id: '22'})
+  .delete()
+  // .update(
+  //   {name: 'Dj Reynolds Pub and Restaurant'}
+  // )
+  // .whereIn('address_zipcode', ['10012','10013','10014'])
+  // .orderBy('name','asec')
+  // .limit(5)
+  // .insert(
+  //   [
+  //     {name: 'Byte Cafe',
+  //       borough: 'Brooklyn',
+  //       cuisine: 'coffee',
+  //       address_building_number: '1234',
+  //       address_street: 'Atlantic Avenue',
+  //       address_zipcode: '11231'},
+  //     {name: 'Byte Cafe 2',
+  //       borough: 'Brooklyn',
+  //       cuisine: 'coffee',
+  //       address_building_number: '12345',
+  //       address_street: 'Atlantic Avenue',
+  //       address_zipcode: '11231'},
+  //     {name: 'Byte Cafe 3',
+  //       borough: 'Brooklyn',
+  //       cuisine: 'coffee',
+  //       address_building_number: '123456',
+  //       address_street: 'Atlantic Avenue',
+  //       address_zipcode: '11231'}
+  //   ]
+  // )
+  // .returning (['id','name'])
   .debug(true)
   .then(results => console.log(JSON.stringify(results, null, 2)));
